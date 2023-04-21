ae9d27fe7533962695a949d60a92214c824d9271
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-21 22:54:23 +0800

    Fetch posts all by myself lol.

---
+++ src/react-query/hooks/usePosts.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+
+const usePosts = () => {
+  return useQuery<Post[], Error>({
+    queryKey: ["posts"],
+    queryFn: () =>
+      axios
+        .get<Post[]>("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => res.data),
+  });
+};
+
+export default usePosts;
