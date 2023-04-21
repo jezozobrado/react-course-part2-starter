9c5d8bf8841b57ceb3bf74be6801fc3be18ad328
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-22 01:34:28 +0800

    Create usable http service

--- src/react-query/hooks/useTodos.ts
+++ src/react-query/hooks/useTodos.ts
@@ -1,21 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+import todoService, { Todo } from "../services/todoService";
 import { CACHE_KEY_TODOS } from "../constants";
-import APIClient from "../services/apiClient";
-
-const apiClient = new APIClient<Todo>("/todos");
-
-export interface Todo {
-  id: number;
-  title: string;
-  userId: number;
-  completed: boolean;
-}
 
 const useTodos = () => {
   return useQuery<Todo[], Error>({
     queryKey: CACHE_KEY_TODOS,
-    queryFn: apiClient.getAll,
+    queryFn: todoService.getAll,
   });
 };
 
