19bd2f1af030e08bbda3b8aa1b2fdda9afc2a01c
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-22 00:09:00 +0800

    Reimplement mutation.

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,15 +1,46 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
+  const queryClient = useQueryClient();
+  const addTodo = useMutation({
+    mutationFn: (todo: Todo) =>
+      axios
+        .post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+        .then((res) => res.data),
+    onSuccess: (savedTodo, newTodo) => {
+      console.log(savedTodo, newTodo);
+      queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+        savedTodo,
+        ...(todos || []),
+      ]);
+    },
+  });
   const ref = useRef<HTMLInputElement>(null);
 
   return (
-    <form className="row mb-3">
+    <form
+      className="row mb-3"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (ref.current && ref.current.value)
+          addTodo.mutate({
+            id: 0,
+            title: ref.current?.value,
+            userId: 1,
+            completed: false,
+          });
+      }}
+    >
       <div className="col">
         <input ref={ref} type="text" className="form-control" />
       </div>
       <div className="col">
-        <button className="btn btn-primary">Add</button>
+        <button type="submit" className="btn btn-primary">
+          Add
+        </button>
       </div>
     </form>
   );
