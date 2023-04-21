d6b9ccacd77becb9170feb65a8955a3586be5d4a
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-21 22:28:05 +0800

    Revert "Fetch todolist"
    This reverts commit 74a16db434818e29fb73084965cdd2237f521fc5.

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,13 @@
-import "bootstrap/dist/css/bootstrap.css";
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import "./index.css";
+import 'bootstrap/dist/css/bootstrap.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const queryClient = new QueryClient();
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
