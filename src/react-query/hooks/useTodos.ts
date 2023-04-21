9cad13b938b0b2e9f86f757f3ffc7fe5e07b2f18
jezozobrado <124932574+jezozobrado@users.noreply.github.com>
2023-04-21 22:43:07 +0800

    Create custom query hook to separate concerns.

---
+++ src/react-query/hooks/useTodos.ts
@@ -0,0 +1,21 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const useTodos = () => {
+  return useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: () =>
+      axios
+        .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+        .then((res) => res.data),
+  });
+};
+
+export default useTodos;
